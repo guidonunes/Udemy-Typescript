537ec6fac9d953d7cda873655b93474b24f05022
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-05 15:37:41 -0300

    add snippet to train syntax w/ examples

--- basic-ts/annotations/variables.ts
+++ basic-ts/annotations/variables.ts
@@ -9,3 +9,17 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+let myNumbers: number[] = [1, 2, 3];
+
+let truths: boolean[] = [true, true, false];
+
+// Classes
+
+class Car {
+
+}
+
+let car: Car = new Car();
