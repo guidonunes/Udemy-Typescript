e76310d8bacaa0d5e80328f4521a51974a253c60
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-07 16:25:13 -0300

    update snippets example

--- basic-ts/annotations/arrays.ts
+++ basic-ts/annotations/arrays.ts
@@ -3,3 +3,5 @@ const carMakes: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
+
+// Help with inference when extracting values
