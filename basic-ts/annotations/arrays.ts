f493ab71e667d2f3e6a85381fb733f761b860dfd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-07 16:38:45 -0300

    update snippets example

--- basic-ts/annotations/arrays.ts
+++ basic-ts/annotations/arrays.ts
@@ -5,3 +5,17 @@ const dates = [new Date(), new Date()];
 const carsByMake: string[][] = [];
 
 // Help with inference when extracting values
+
+const car = carMakes[0];
+const myCar = carMakes.pop();
+
+// Prevent incompatible values
+carMakes.push('mercedes');
+
+// Help with 'map'
+carMakes.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()];
