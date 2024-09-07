5fb4bed9b3e8db06e6b99445f94da9f350bfd57a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-07 17:21:23 -0300

    update snippets example

--- basic-ts/annotations/interfaces.ts
+++ basic-ts/annotations/interfaces.ts
@@ -2,6 +2,7 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 
@@ -9,13 +10,14 @@ const civic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
 }
 
 printVehicle(civic);
