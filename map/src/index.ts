6207f50d4b95e65412a7d608c05455e82215fe61
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-11 15:38:31 -0300

    fix api key publicity

--- map/src/index.ts
+++ map/src/index.ts
@@ -2,6 +2,16 @@
 import { User } from './User';
 import { Company } from './Company';
 
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+function loadGoogleMaps() {
+  const script = document.createElement('script');
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap`;
+  document.head.appendChild(script);
+}
+
+loadGoogleMaps();
+
 const user = new User();
 const company = new Company();
 
